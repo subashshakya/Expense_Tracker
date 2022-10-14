66ad42dd991798d38059001fdbdc5cdcee076eae
subashshakya <subashshakya12@gmail.com>
2022-10-14 19:05:40 +0545

    Creating Model Class

---
+++ expense_tracker/lib/models/transaction.dart
@@ -0,0 +1,14 @@
+//creating models
+
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction(
+      {required this.id,
+      required this.title,
+      required this.amount,
+      required this.date});
+}
